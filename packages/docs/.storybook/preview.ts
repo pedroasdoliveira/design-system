fe8fae488a02c73b2ab8f81aa7b7bcc0f24fa352
Pedro Oliveira <pedroaolive@gmail.com>
2025-04-17 19:15:20 -0300

    add config for background colors in Storybook

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,12 +1,23 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
+    },
+    backgrounds: {
+      values: [
+        // 👇 Default values
+        { name: "Dark", value: "#333" },
+        { name: "Light", value: "#F7F9F2" },
+        // 👇 Add your own
+        { name: "Maroon", value: "#400" },
+      ],
+      // 👇 Specify which background is shown by default
+      default: "Light",
     },
   },
 };
